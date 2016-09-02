fa21d566b03f5f12f3b9afb50769fde046253254
Rogue Andy <zlj_1969_love@126.com>
2016-09-02 14:55:18 +0800

    播放音频的文件(空)

---
+++ RgFeinno/RgFeinno/RgSound/RgSoundPlay.h
@@ -0,0 +1,13 @@
+//
+//  RgSoundPlay.h
+//  RgFeinno
+//
+//  Created by Rogue Andy on 16/9/2.
+//  Copyright © 2016年 RogueAndy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RgSoundPlay : NSObject
+
+@end
---
+++ RgFeinno/RgFeinno/RgSound/RgSoundPlay.m
@@ -0,0 +1,20 @@
+//
+//  RgSoundPlay.m
+//  RgFeinno
+//
+//  Created by Rogue Andy on 16/9/2.
+//  Copyright © 2016年 RogueAndy. All rights reserved.
+//
+
+#import "RgSoundPlay.h"
+#import "FSAudioStream.h"
+
+@interface RgSoundPlay()
+
+//@property (nonatomic, strong) f
+
+@end
+
+@implementation RgSoundPlay
+
+@end
